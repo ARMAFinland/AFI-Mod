99cf0178438d8a6536541af7f1718a1233e62d29
tuntematonjr <mika.lieto@hotmail.com>
2024-08-17 20:25:09 +0300

    initial release

---
+++ mod.cpp
@@ -0,0 +1,27 @@
+dir = "@AFI Tweaks";
+name = "AFI Tweaks v0.0.0";
+
+author = "Tuntematon";
+
+picture         = "data\afilogo.paa";           // Picture displayed from the expansions menu. Optimal size is 2048x1024
+hideName        = "false";                  // Hide the extension name
+hidePicture     = "false";                  // Hide the extension menu
+
+logoSmall       = "data\afilogo.paa";         // Display next to the item added by the mod 64x64
+logo            = "data\afilogo.paa";        // Logo displayed in the main menu 128x128
+logoOver        = "data\afilogo.paa"; // When the mouse is over, in the main menu 128x128
+tooltip         = "Tweaks and compatibilities";
+tooltipOwned    = "Tweaks and compatibilities";  // Tool tip displayed when the mouse is left over, in the main menu
+overview        = "AFI Tweaks v0.0.0";         // Overview text, displayed from the extension menu
+description     = "It's unclear where this will show"; // Probably in context with action
+action          = "https://github.com/ARMAFinland/AFI-Tweaks";    // Website URL, that can accessed from the expansions menu 
+actionName      = "Github";                 // label of button/tooltip in extension menu
+
+// Color used for DLC stripes and backgrounds (RGBA)
+dlcColor[] =
+{
+    1,
+    0.0,
+    0.86,
+    1
+};
