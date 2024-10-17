d39ff3ab04bc8b179bc2c7d97ca6d46988e5c130
tuntematonjr <mika.lieto@hotmail.com>
2024-10-17 18:22:46 +0300

    Add afi logo

---
+++ addons/logo/config.cpp
@@ -0,0 +1,175 @@
+#include "script_component.hpp"
+
+// information on this addon specifically
+class CfgPatches {
+	class ADDON {
+		name = COMPONENT_NAME;
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = REQUIRED_VERSION;
+		requiredAddons[] = {MAIN_ADDON_STR,"A3_Ui_F","A3_Data_F_Enoch_Loadorder"};
+		author = "Tuntematon";
+		authorUrl = GITHUBLINK;
+		VERSION_CONFIG;
+	};
+};
+// configs go here
+//#include "CfgEventHandlers.hpp"
+
+class RscPictureKeepAspect;
+class RscStandardDisplay;
+class RscControlsGroup;
+class RscDisplayStart: RscStandardDisplay
+{
+	class controls
+	{
+		class LoadingStart: RscControlsGroup
+		{
+			class controls
+			{
+				class Logo: RscPictureKeepAspect
+				{
+					//onLoad = QUOTE((_this select 0) ctrlsettext PATHTOF(afilogo.paa););
+					onLoad = "_this select 0 ctrlsettext '\x\afitweaks\addons\logo\afilogo.paa';";
+					
+				};
+			};
+		};
+	};
+};
+class RscDisplayLoadMission: RscStandardDisplay
+{
+	class controls
+	{
+		class LoadingStart: RscControlsGroup
+		{
+			class controls
+			{
+				class Logo: RscPictureKeepAspect
+				{
+					//onLoad = QUOTE(_this select 0 ctrlsettext PATHTOF(afilogo.paa););
+					onLoad = "_this select 0 ctrlsettext '\x\afitweaks\addons\logo\afilogo.paa';";
+				};
+			};
+		};
+	};
+};
+class RscDisplayNotFreeze: RscStandardDisplay
+{
+	class controls
+	{
+		class LoadingStart: RscControlsGroup
+		{
+			class controls
+			{
+				class Logo: RscPictureKeepAspect
+				{
+					//onLoad = QUOTE(_this select 0 ctrlsettext "\x\afitweaks\addons\logo\afilogo.paa";);
+					onLoad = "_this select 0 ctrlsettext '\x\afitweaks\addons\logo\afilogo.paa';";
+				};
+			};
+		};
+	};
+};
+class RscActivePictureKeepAspect;
+class RscButton;
+class RscDisplayMain: RscStandardDisplay
+{
+	class Controls
+	{
+		// This would change the arma logo at top
+		// class Logo: RscActivePictureKeepAspect
+		// {
+		// 	tooltip = "Connect to AFI event server";
+		// 	text = QPATHTOF(afilogo.paa);
+		// 	onButtonClick = "connectToServer ['65.21.188.102', 2202, 'kotka'];";
+		// };
+		// class LogoApex: Logo
+		// {
+		// 	tooltip = "Connect to AFI event server";
+		// 	text = QPATHTOF(afilogo.paa);
+		// 	onButtonClick = "connectToServer ['65.21.188.102', 2202, 'kotka'];";
+		// };
+		class GVAR(serveriLeftText): RscButton
+		{
+			text = "Join AFI weekly games server";
+			//tooltip = "1";
+			style = QUOTE(2 + 192);//  
+			SizeEx = QUOTE(GUI_TEXT_SIZE_LARGE);
+			//onButtonClick = "connectToServer ['65.21.188.102', 2202, 'kotka'];";
+			x = "0.5 - (1.5 * 	10) * 	(pixelW * pixelGridNoUIScale * 2) - 	(2 * pixelW)";
+			y = "0.37 - (	10 / 2) * 	(pixelH * pixelGridNoUIScale * 2)";
+			w = "10 * 	(pixelW * pixelGridNoUIScale * 2)";
+			h = "1 * 	(pixelH * pixelGridNoUIScale * 2)";
+			//color[] = {1,1,1,1};
+			colorBackground[] = {0,0,0,1};
+			background = 1;
+		};
+		class GVAR(serveriCenterText): GVAR(serveriLeftText)
+		{
+			text = "Join AFI main event server";
+			tooltip = "1";
+			x = "0.5 - (0.5 * 	10) * 	(pixelW * pixelGridNoUIScale * 2)";
+
+		};
+		class GVAR(serveriRightText): GVAR(serveriLeftText)
+		{
+			text = "Join AFI secondary event server";
+			tooltip = "1";
+			x = "0.5 + (0.5 * 	10) * 	(pixelW * pixelGridNoUIScale * 2) + 	(2 * pixelW)";
+		};
+		class GVAR(serveriLeftImage): RscActivePictureKeepAspect
+		{
+			text = QPATHTOF(afilogo.paa);
+			tooltip = "This is afi weekly game server";
+			onButtonClick = "connectToServer ['65.21.188.102', 2302, 'kotka'];";
+			x = "0.5 - (1.5 * 	10) * 	(pixelW * pixelGridNoUIScale * 2) - 	(2 * pixelW)";
+			y = "0.01 - (	10 / 2) * 	(pixelH * pixelGridNoUIScale * 2)";
+			w = "10 * 	(pixelW * pixelGridNoUIScale * 2)";
+			h = "7 * 	(pixelH * pixelGridNoUIScale * 2)";
+			color[] = {1,1,1,1};
+		};
+
+		class GVAR(serveriCenterImage): GVAR(serveriLeftImage)
+		{
+			text = QPATHTOF(afilogo.paa);
+			tooltip = "This is main afi event server, all our events use this if not told otherwise.";
+			onButtonClick = "connectToServer ['65.21.188.102', 2202, 'kotka'];";
+			x = "0.5 - (0.5 * 	10) * 	(pixelW * pixelGridNoUIScale * 2)";
+		};
+
+		class GVAR(serveriRightImage): GVAR(serveriLeftImage)
+		{
+			text = QPATHTOF(afilogo.paa);
+			tooltip = "Almost never used, just in case as backup here.";
+			onButtonClick = "connectToServer ['65.21.188.102', 2502, 'kotka'];";
+			x = "0.5 + (0.5 * 	10) * 	(pixelW * pixelGridNoUIScale * 2) + 	(2 * pixelW)";
+		};
+	};
+};
+
+
+//This would add the spotlight thing, it was bit buggy.
+// class CfgMainMenuSpotlight
+// {
+// 	class GVAR(eventServer)
+// 	{
+// 		text = "AFI Event server";
+// 		textIsQuote = 0;
+// 		picture = QPATHTOF(afilogo.paa);
+// 		video = "";
+// 		action = "connectToServer ['65.21.188.102', 2202, 'kotka'];";
+// 		actionText = "Connect to Session";
+// 		condition = "true";
+// 	};
+// 	class GVAR(weeklyGameServer)
+// 	{
+// 		text = "AFI weekly game server";
+// 		textIsQuote = 0;
+// 		picture = QPATHTOF(afilogo.paa);
+// 		video = "";
+// 		action = "connectToServer ['65.21.188.102', 2302, 'kotka'];";
+// 		actionText = "Connect to Session";
+// 		condition = "true";
+// 	};
+// };
---
+++ addons/logo/script_component.hpp
@@ -0,0 +1,60 @@
+#define COMPONENT logo
+#define COMPONENT_BEAUTIFIED logo
+#include "\x\afitweaks\addons\main\script_mod.hpp"
+
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+
+#ifdef DEBUG_ENABLED_MAIN
+    #define DEBUG_MODE_FULL
+#endif
+
+#include "\x\afitweaks\addons\main\script_macros.hpp"
+
+
+
+
+// Static styles
+#define ST_POS            0x0F
+#define ST_HPOS           0x03
+#define ST_VPOS           0x0C
+#define ST_LEFT           0x00
+#define ST_RIGHT          0x01
+#define ST_CENTER         0x02
+#define ST_DOWN           0x04
+#define ST_UP             0x08
+#define ST_VCENTER        0x0C
+
+#define ST_TYPE           0xF0
+#define ST_SINGLE         0x00
+#define ST_MULTI          0x10
+#define ST_TITLE_BAR      0x20
+#define ST_PICTURE        0x30
+#define ST_FRAME          0x40
+#define ST_BACKGROUND     0x50
+#define ST_GROUP_BOX      0x60
+#define ST_GROUP_BOX2     0x70
+#define ST_HUD_BACKGROUND 0x80
+#define ST_TILE_PICTURE   0x90
+#define ST_WITH_RECT      0xA0
+#define ST_LINE           0xB0
+#define ST_UPPERCASE      0xC0
+#define ST_LOWERCASE      0xD0
+
+#define ST_SHADOW         0x100
+#define ST_NO_RECT        0x200
+#define ST_KEEP_ASPECT_RATIO  0x800
+
+
+// Default grid
+#define GUI_GRID_WAbs			((safezoneW / safezoneH) min 1.2)
+#define GUI_GRID_HAbs			(GUI_GRID_WAbs / 1.2)
+#define GUI_GRID_W			(GUI_GRID_WAbs / 40)
+#define GUI_GRID_H			(GUI_GRID_HAbs / 25)
+#define GUI_GRID_X			(safezoneX)
+#define GUI_GRID_Y			(safezoneY + safezoneH - GUI_GRID_HAbs)
+
+// Default text sizes
+#define GUI_TEXT_SIZE_SMALL		(GUI_GRID_H * 0.8)
+#define GUI_TEXT_SIZE_MEDIUM		(GUI_GRID_H * 1)
+#define GUI_TEXT_SIZE_LARGE		(GUI_GRID_H * 1.2)
