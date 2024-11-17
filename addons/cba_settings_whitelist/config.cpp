8ba43cde21224fe3836e61b46dbb853628a9c9df
tuntematonjr <mika.lieto@hotmail.com>
2024-11-17 19:38:46 +0200

    add cba settings whitelist

---
+++ addons/cba_settings_whitelist/config.cpp
@@ -0,0 +1,18 @@
+#include "script_component.hpp"
+
+// information on this addon specifically
+class CfgPatches {
+	class ADDON {
+		name = COMPONENT_NAME;
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = REQUIRED_VERSION;
+		requiredAddons[] = {MAIN_ADDON_STR,"CBA_settings"};
+		author[] = {"Tikka"};
+		authorUrl = GITHUBLINK;
+		VERSION_CONFIG;
+		skipWhenMissingDependencies = 1;
+	};
+};
+// configs go here
+cba_settings_whitelist[] = {"76561198002057694", "76561197976768806", "76561198039338079","76561197990347153", "76561197972154686", "76561197990633198", "76561198039162445"};
