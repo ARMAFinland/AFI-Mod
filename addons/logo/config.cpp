4a39a4b7f814953f6a232d3adbd41a09ed486cab
tuntematonjr <mika.lieto@hotmail.com>
2024-11-20 10:28:46 +0200

    update logo macros

--- addons/logo/config.cpp
+++ addons/logo/config.cpp
@@ -19,53 +19,34 @@ class CfgPatches {
 class RscPictureKeepAspect;
 class RscStandardDisplay;
 class RscControlsGroup;
-class RscDisplayStart: RscStandardDisplay
-{
-	class controls
-	{
-		class LoadingStart: RscControlsGroup
-		{
-			class controls
-			{
-				class Logo: RscPictureKeepAspect
-				{
-					//onLoad = QUOTE((_this select 0) ctrlsettext PATHTOF(afilogo.paa););
-					onLoad = "_this select 0 ctrlsettext '\x\afitweaks\addons\logo\afilogo.paa';";
-					
+class RscDisplayStart: RscStandardDisplay {
+	class controls {
+		class LoadingStart: RscControlsGroup {
+			class controls {
+				class Logo: RscPictureKeepAspect {
+					onLoad = QUOTE(_this select 0 ctrlsettext QUOTE(QPATHTOF(afilogo.paa)););
 				};
 			};
 		};
 	};
 };
-class RscDisplayLoadMission: RscStandardDisplay
-{
-	class controls
-	{
-		class LoadingStart: RscControlsGroup
-		{
-			class controls
-			{
-				class Logo: RscPictureKeepAspect
-				{
-					//onLoad = QUOTE(_this select 0 ctrlsettext PATHTOF(afilogo.paa););
-					onLoad = "_this select 0 ctrlsettext '\x\afitweaks\addons\logo\afilogo.paa';";
+class RscDisplayLoadMission: RscStandardDisplay {
+	class controls{
+		class LoadingStart: RscControlsGroup {
+			class controls {
+				class Logo: RscPictureKeepAspect {
+					onLoad = QUOTE(_this select 0 ctrlsettext QUOTE(QPATHTOF(afilogo.paa)););
 				};
 			};
 		};
 	};
 };
-class RscDisplayNotFreeze: RscStandardDisplay
-{
-	class controls
-	{
-		class LoadingStart: RscControlsGroup
-		{
-			class controls
-			{
-				class Logo: RscPictureKeepAspect
-				{
-					//onLoad = QUOTE(_this select 0 ctrlsettext "\x\afitweaks\addons\logo\afilogo.paa";);
-					onLoad = "_this select 0 ctrlsettext '\x\afitweaks\addons\logo\afilogo.paa';";
+class RscDisplayNotFreeze: RscStandardDisplay {
+	class controls {
+		class LoadingStart: RscControlsGroup {
+			class controls {
+				class Logo: RscPictureKeepAspect {
+					onLoad = QUOTE(_this select 0 ctrlsettext QUOTE(QPATHTOF(afilogo.paa)););
 				};
 			};
 		};
@@ -73,41 +54,35 @@ class RscDisplayNotFreeze: RscStandardDisplay
 };
 class RscActivePictureKeepAspect;
 class RscButton;
-class RscDisplayMain: RscStandardDisplay
-{
-	class ControlsBackground
-	{
-		class GVAR(serveriLeftImage): RscActivePictureKeepAspect
-		{
+class RscDisplayMain: RscStandardDisplay{
+	class ControlsBackground {
+		class GVAR(serveriLeftImage): RscActivePictureKeepAspect {
 			text = QPATHTOF(afilogo.paa);
 			//tooltip = "This is afi weekly game server";
 			//onButtonClick = "connectToServer ['65.21.188.102', 2302, 'kotka'];";
-			x = "0.5 - (1.5 * 	10) * 	(pixelW * pixelGridNoUIScale * 2) - 	(2 * pixelW)";
-			y = "0.01 - (	10 / 2) * 	(pixelH * pixelGridNoUIScale * 2)";
-			w = "10 * 	(pixelW * pixelGridNoUIScale * 2)";
-			h = "7 * 	(pixelH * pixelGridNoUIScale * 2)";
+			x = "0.5 - (1.5 * 10) * (pixelW * pixelGridNoUIScale * 2) - (2 * pixelW)";
+			y = "0.01 - (10 / 2) * (pixelH * pixelGridNoUIScale * 2)";
+			w = "10 * (pixelW * pixelGridNoUIScale * 2)";
+			h = "7 * (pixelH * pixelGridNoUIScale * 2)";
 			color[] = {1,1,1,1};
 			background = 1;
 		};
 
-		class GVAR(serveriCenterImage): GVAR(serveriLeftImage)
-		{
+		class GVAR(serveriCenterImage): GVAR(serveriLeftImage) {
 			text = QPATHTOF(afilogo.paa);
 			//tooltip = "This is main afi event server, all our events use this if not told otherwise.";
 			//onButtonClick = "connectToServer ['65.21.188.102', 2202, 'kotka'];";
-			x = "0.5 - (0.5 * 	10) * 	(pixelW * pixelGridNoUIScale * 2)";
+			x = "0.5 - (0.5 * 10) * (pixelW * pixelGridNoUIScale * 2)";
 		};
 
-		class GVAR(serveriRightImage): GVAR(serveriLeftImage)
-		{
+		class GVAR(serveriRightImage): GVAR(serveriLeftImage) {
 			text = QPATHTOF(afilogo.paa);
 			//tooltip = "Almost never used, just in case as backup here.";
 			//onButtonClick = "connectToServer ['65.21.188.102', 2502, 'kotka'];";
-			x = "0.5 + (0.5 * 	10) * 	(pixelW * pixelGridNoUIScale * 2) + 	(2 * pixelW)";
+			x = "0.5 + (0.5 * 10) * (pixelW * pixelGridNoUIScale * 2) + (2 * pixelW)";
 		};
 	};
-	class Controls
-	{
+	class Controls {
 		// This would change the arma logo at top
 		// class Logo: RscActivePictureKeepAspect
 		// {
@@ -121,34 +96,31 @@ class RscDisplayMain: RscStandardDisplay
 		// 	text = QPATHTOF(afilogo.paa);
 		// 	onButtonClick = "connectToServer ['65.21.188.102', 2202, 'kotka'];";
 		// };
-		class GVAR(serveriLeftText): RscButton
-		{
+		class GVAR(serveriLeftText): RscButton {
 			text = "Join AFI weekly games server";
 			tooltip = "This is afi weekly game server";
 			style = QUOTE(2 + 192);
 			SizeEx = QUOTE(GUI_TEXT_SIZE_LARGE);
 			onButtonClick = "connectToServer ['65.21.188.102', 2302, 'kotka'];";
-			x = "0.5 - (1.5 * 	10) * 	(pixelW * pixelGridNoUIScale * 2) - 	(2 * pixelW)";
-			y = "0.37 - (	10 / 2) * 	(pixelH * pixelGridNoUIScale * 2)";
-			w = "10 * 	(pixelW * pixelGridNoUIScale * 2)";
-			h = "1 * 	(pixelH * pixelGridNoUIScale * 2)";
+			x = "0.5 - (1.5 * 10) * (pixelW * pixelGridNoUIScale * 2) - (2 * pixelW)";
+			y = "0.37 - (10 / 2) * (pixelH * pixelGridNoUIScale * 2)";
+			w = "10 * pixelW * pixelGridNoUIScale * 2)";
+			h = "1 * (pixelH * pixelGridNoUIScale * 2)";
 			//color[] = {1,1,1,1};
 			colorBackground[] = {0,0,0,1};
 		};
-		class GVAR(serveriCenterText): GVAR(serveriLeftText)
-		{
+		class GVAR(serveriCenterText): GVAR(serveriLeftText) {
 			text = "Join AFI main event server";
 			tooltip = "This is main afi event server, all our events use this if not told otherwise.";
 			onButtonClick = "connectToServer ['65.21.188.102', 2202, 'kotka'];";
-			x = "0.5 - (0.5 * 	10) * 	(pixelW * pixelGridNoUIScale * 2)";
+			x = "0.5 - (0.5 * 10) * (pixelW * pixelGridNoUIScale * 2)";
 
 		};
-		class GVAR(serveriRightText): GVAR(serveriLeftText)
-		{
+		class GVAR(serveriRightText): GVAR(serveriLeftText) {
 			text = "Join AFI secondary event server";
 			tooltip = "Almost never used, just in case as backup here.";
 			onButtonClick = "connectToServer ['65.21.188.102', 2502, 'kotka'];";
-			x = "0.5 + (0.5 * 	10) * 	(pixelW * pixelGridNoUIScale * 2) + 	(2 * pixelW)";
+			x = "0.5 + (0.5 * 10) * (pixelW * pixelGridNoUIScale * 2) + (2 * pixelW)";
 		};
 	};
 };
