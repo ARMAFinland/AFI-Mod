80f01b3ca231c327cfbeb2470683df2a6cb36250
tuntematonjr <mika.lieto@hotmail.com>
2024-11-28 20:26:29 +0200

    Update mod.cpp

--- mod.cpp
+++ mod.cpp
@@ -1,5 +1,5 @@
-dir = "@AFI Tweaks";
-name = "AFI Tweaks v0.0.0";
+dir = "@afi";
+name = "AFI v0.0.0";
 
 author = "Tuntematon";
 
@@ -12,9 +12,9 @@ logo            = "data\afilogo.paa";        // Logo displayed in the main menu 
 logoOver        = "data\afilogo.paa"; // When the mouse is over, in the main menu 128x128
 tooltip         = "Tweaks and compatibilities";
 tooltipOwned    = "Tweaks and compatibilities";  // Tool tip displayed when the mouse is left over, in the main menu
-overview        = "AFI Tweaks v0.0.0";         // Overview text, displayed from the extension menu
+overview        = "AFI v0.0.0";         // Overview text, displayed from the extension menu
 description     = "It's unclear where this will show"; // Probably in context with action
-action          = "https://github.com/ARMAFinland/AFI-Tweaks";    // Website URL, that can accessed from the expansions menu 
+action          = "https://github.com/ARMAFinland/AFI-Mod";    // Website URL, that can accessed from the expansions menu 
 actionName      = "Github";                 // label of button/tooltip in extension menu
 
 // Color used for DLC stripes and backgrounds (RGBA)
