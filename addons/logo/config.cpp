2e74c1a9eaaaae5159c006dfcf8291242ddd4773
tuntematonjr <mika.lieto@hotmail.com>
2025-01-27 20:42:14 +0200

    Clean

--- addons/logo/config.cpp
+++ addons/logo/config.cpp
@@ -60,8 +60,6 @@ class RscDisplayMain: RscStandardDisplay{
 	class ControlsBackground {
 		class GVAR(serveriLeftImage): RscActivePictureKeepAspect {
 			text = QPATHTOF(afilogo.paa);
-			//tooltip = "This is afi weekly game server";
-			//onButtonClick = "connectToServer ['65.21.188.102', 2302, 'kotka'];";
 			x = "0.5 - (1.5 * 10) * (pixelW * pixelGridNoUIScale * 2) - (2 * pixelW)";
 			y = "0.01 - (10 / 2) * (pixelH * pixelGridNoUIScale * 2)";
 			w = "10 * (pixelW * pixelGridNoUIScale * 2)";
@@ -72,32 +70,15 @@ class RscDisplayMain: RscStandardDisplay{
 
 		class GVAR(serveriCenterImage): GVAR(serveriLeftImage) {
 			text = QPATHTOF(afilogo.paa);
-			//tooltip = "This is main afi event server, all our events use this if not told otherwise.";
-			//onButtonClick = "connectToServer ['65.21.188.102', 2202, 'kotka'];";
 			x = "0.5 - (0.5 * 10) * (pixelW * pixelGridNoUIScale * 2)";
 		};
 
 		class GVAR(serveriRightImage): GVAR(serveriLeftImage) {
 			text = QPATHTOF(afilogo.paa);
-			//tooltip = "Almost never used, just in case as backup here.";
-			//onButtonClick = "connectToServer ['65.21.188.102', 2502, 'kotka'];";
 			x = "0.5 + (0.5 * 10) * (pixelW * pixelGridNoUIScale * 2) + (2 * pixelW)";
 		};
 	};
 	class Controls {
-		// This would change the arma logo at top
-		// class Logo: RscActivePictureKeepAspect
-		// {
-		// 	tooltip = "Connect to AFI event server";
-		// 	text = QPATHTOF(afilogo.paa);
-		// 	onButtonClick = "connectToServer ['65.21.188.102', 2202, 'kotka'];";
-		// };
-		// class LogoApex: Logo
-		// {
-		// 	tooltip = "Connect to AFI event server";
-		// 	text = QPATHTOF(afilogo.paa);
-		// 	onButtonClick = "connectToServer ['65.21.188.102', 2202, 'kotka'];";
-		// };
 		class GVAR(serveriLeftText): RscButton {
 			text = "Join AFI weekly games server";
 			tooltip = "This is afi weekly game server";
@@ -108,7 +89,6 @@ class RscDisplayMain: RscStandardDisplay{
 			y = "0.37 - (10 / 2) * (pixelH * pixelGridNoUIScale * 2)";
 			w = "10 * pixelW * pixelGridNoUIScale * 2)";
 			h = "1 * (pixelH * pixelGridNoUIScale * 2)";
-			//color[] = {1,1,1,1};
 			colorBackground[] = {0,0,0,1};
 		};
 		class GVAR(serveriCenterText): GVAR(serveriLeftText) {
@@ -126,29 +106,3 @@ class RscDisplayMain: RscStandardDisplay{
 		};
 	};
 };
-
-
-//This would add the spotlight thing, it was bit buggy.
-// class CfgMainMenuSpotlight
-// {
-// 	class GVAR(eventServer)
-// 	{
-// 		text = "AFI Event server";
-// 		textIsQuote = 0;
-// 		picture = QPATHTOF(afilogo.paa);
-// 		video = "";
-// 		action = "connectToServer ['65.21.188.102', 2202, 'kotka'];";
-// 		actionText = "Connect to Session";
-// 		condition = "true";
-// 	};
-// 	class GVAR(weeklyGameServer)
-// 	{
-// 		text = "AFI weekly game server";
-// 		textIsQuote = 0;
-// 		picture = QPATHTOF(afilogo.paa);
-// 		video = "";
-// 		action = "connectToServer ['65.21.188.102', 2302, 'kotka'];";
-// 		actionText = "Connect to Session";
-// 		condition = "true";
-// 	};
-// };
