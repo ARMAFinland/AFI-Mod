e36f932b60d934c0c8eaa8c59a40539bd64de9a0
tuntematonjr <mika.lieto@hotmail.com>
2025-12-02 19:02:34 +0200

    add summaW snow fix

---
+++ addons/summaw/script_component.hpp
@@ -0,0 +1,12 @@
+#define COMPONENT summaw
+#define COMPONENT_BEAUTIFIED summaw
+#include "\x\afi\addons\main\script_mod.hpp"
+
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+
+#ifdef DEBUG_ENABLED_MAIN
+	#define DEBUG_MODE_FULL
+#endif
+
+#include "\x\afi\addons\main\script_macros.hpp"
