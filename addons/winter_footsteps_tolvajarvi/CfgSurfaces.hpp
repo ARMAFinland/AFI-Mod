2eb8671b61ca3ee4aae1308b96d2ad0d5cc16a50
tuntematonjr <mika.lieto@hotmail.com>
2026-03-17 18:44:41 +0200

    winter

---
+++ addons/winter_footsteps_tolvajarvi/CfgSurfaces.hpp
@@ -0,0 +1,21 @@
+class CfgSurfaces {
+	class Default {};
+	class annd_tolvajarvi_hakkuu_Surface: Default {
+		soundEnviron="mfive_winter_snow_soundmod";
+	};
+	class annd_tolvajarvi_kivinen_Surface: Default {
+		soundEnviron="mfive_winter_snow_soundmod";
+	};
+	class annd_tolvajarvi_metsatie_Surface: Default {
+		soundEnviron="mfive_winter_snow_soundmod";
+	};
+	class annd_tolvajarvi_snow2_Surface: Default {
+		soundEnviron="mfive_winter_snow_soundmod";
+	};
+	class annd_tolvajarvi_snow_Surface: Default {
+		soundEnviron="mfive_winter_snow_soundmod";
+	};
+	class annd_tolvajarvi_suo_Surface: Default {
+		soundEnviron="mfive_winter_snow_soundmod";
+	};
+};
